92f8b00c2bba5f4ef69be2f5972e0a5be04cc2fb
Scott Morrison <scott@tqft.net>
2015-05-10 22:00:21 +1000

    Cleaning up Time

--- src/main/scala/org/mathoverflow/archive/Time.scala
+++ src/main/scala/org/mathoverflow/archive/Time.scala
@@ -6,37 +6,42 @@ import java.util.TimeZone
 import java.util.Date
 
 object Time {
+  private val longFormat = new SimpleDateFormat("yyyyMMdd/HHmm")
+  private val shortFormat = new SimpleDateFormat("yyyyMMdd")
+  longFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+  shortFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+  private val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+
   def now = {
-    val format = new SimpleDateFormat("yyyyMMdd/HHmm")
-    format.setTimeZone(TimeZone.getTimeZone("UTC"))
-    parse(format.format(new Date))
+    parse(longFormat.format(new Date))
   }
   def parse(date: String): Long = {
-    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-    date.split("/").toSeq match {
-      case Seq("") => {
+    try {
+      calendar.clear()
+      date.split("/").toSeq match {
+        case Seq("") => {
+          now
+        }
+        case Seq(date) => {
+          calendar.setTime(shortFormat.parse(date));
+          calendar.getTimeInMillis() / 1000
+        }
+        case Seq(date, time) => {
+          calendar.setTime(longFormat.parse(date + "/" + time));
+          calendar.getTimeInMillis() / 1000
+        }
+      }
+    } catch {
+      case e: Exception => {
+        // fail as best we can
         now
-      }
-      case Seq(date) => {
-        calendar.clear()
-        val format = new SimpleDateFormat("yyyyMMdd")
-        calendar.setTime(format.parse(date));
-        calendar.getTimeInMillis() / 1000
-      }
-      case Seq(date, time) => {
-        calendar.clear()
-        val format = new SimpleDateFormat("yyyyMMdd/HHmm")
-        calendar.setTime(format.parse(date + "/" + time));
-        calendar.getTimeInMillis() / 1000
       }
     }
   }
-  private val format = new SimpleDateFormat("yyyyMMdd/HHmm");
   def write(date: Long): String = {
-    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
     calendar.clear();
     calendar.setTimeInMillis(date * 1000)
-    format.format(calendar.getTime)
+    longFormat.format(calendar.getTime)
   }
   def round(date: Long): Long = parse(write(date))
 }
